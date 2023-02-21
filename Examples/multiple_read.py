bf34ddd69777d838f5815fc9a67708276172627a
sbshop1 <96284682+sbcshop1@users.noreply.github.com>
2023-02-21 15:48:39 +0530

    Add files via upload

---
+++ Examples/multiple_read.py
@@ -0,0 +1,22 @@
+from machine import UART, Pin,SPI,I2C
+import time,utime
+from uhf import UHF
+enable_pin = machine.Pin(4, machine.Pin.OUT)
+enable_pin.value(0)
+
+baudrate = 115200
+uhf = UHF(baudrate)
+uhf.multiple_read()
+try:
+    while 1:
+        rev = uhf.read_mul()
+        if rev is not None:
+            print(rev)
+            print('RSSI(dBm) = ',rev[5])
+            print('CRC = ',rev[20],rev[21])
+        time.sleep(0.0009)
+except KeyboardInterrupt:
+    uhf.stop_read()
+    time.sleep(1)
+    print("stop")
+   
