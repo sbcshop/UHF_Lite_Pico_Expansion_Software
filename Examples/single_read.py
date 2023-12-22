69949f12923d2746cc38a5f6a0cb13f99fdf4f82
sbshop1 <96284682+sbcshop1@users.noreply.github.com>
2023-12-22 11:36:57 +0530

    Update single_read.py

--- Examples/single_read.py
+++ Examples/single_read.py
@@ -6,6 +6,14 @@ enable_pin.value(0)
 
 baudrate = 115200
 uhf = UHF(baudrate)
+
+'''
+Uncomment corresponding section to increase reading range,
+you will have to set the region as per requirment
+'''
+#uhf.setRegion_EU() 
+#uhf.setRegion_US()
+
 rev = uhf.single_read()
 if rev is not None:
    print('EPC = ',rev[8:20])
