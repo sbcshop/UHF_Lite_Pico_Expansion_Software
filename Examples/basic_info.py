39fae02c87bc2683ae5e425b1e1208c7b4ed0bd8
sbshop1 <96284682+sbcshop1@users.noreply.github.com>
2023-12-22 11:37:57 +0530

    Update basic_info.py

--- Examples/basic_info.py
+++ Examples/basic_info.py
@@ -6,5 +6,13 @@ enable_pin.value(0)
 
 baudrate = 115200
 uhf = UHF(baudrate)
+
+'''
+Uncomment corresponding section to increase reading range,
+you will have to set the region as per requirment
+'''
+#uhf.setRegion_EU() 
+#uhf.setRegion_US()
+
 hw = uhf.hardware_version()
 print('Hardware Version:',hw)
